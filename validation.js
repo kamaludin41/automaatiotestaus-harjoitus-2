2652696cec767df74637cd81b66931c655a8b65e
kamaludin41 <GitHub-tiliss kyttmsi shkpostiosoite>
2026-09-04 12:40:16 +0300

    Tehtävä 8: korjaa validateEmail

--- validation.js
+++ validation.js
@@ -1,16 +1,11 @@
 export function validateEmail(email) {
-  return typeof email === "string" &&
-    email.includes("@") &&
-    email.includes(".");
+  return typeof email === "string" && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
 export function validatePassword(password) {
-  return typeof password === "string" &&
-    password.length >= 8;
+  return typeof password === "string" && password.length >= 8;
 }
 
 export function validateAge(age) {
-  return Number.isInteger(age) &&
-    age >= 16 &&
-    age <= 120;
+  return Number.isInteger(age) && age >= 16 && age <= 120;
 }
